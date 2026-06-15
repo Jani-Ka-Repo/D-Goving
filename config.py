12b3f6af52198f55c351ccbe6478a7f0c8e86b4a
Jani-Ka-Repo <santidevi654378@gmail.com>
2026-06-15 22:51:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Jani-Papa-Repo-RP/OP",
+    "https://github.com/Jani-Ka-Repo/Clone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
